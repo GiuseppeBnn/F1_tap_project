3ec110770e3ffe1189992898138b0f2d58586108
GiuseppeBnn <giusbon2@gmail.com>
2023-05-30 22:11:55 +0200

    added script & compose

---
+++ ReplayProducerCloud/F1SessionReplayProducer.py
@@ -0,0 +1,153 @@
+from dateutil import parser
+import time
+import json
+import requests as req
+from confluent_kafka import Producer
+
+def jsonModifier(jsonData, pilotsNumber,recapBool):
+    if(recapBool):
+        jsonData = jsonData["R"]["TimingData"]["Lines"][str(pilotsNumber)]
+        jsonData["PilotNumber"]=str(pilotsNumber)
+    else:
+        jsonData = jsonData["M"][0]["A"][1]["Lines"][str(pilotsNumber)]
+        jsonData["PilotNumber"]=str(pilotsNumber)
+        
+    return jsonData
+
+
+
+
+
+def read_ccloud_config(config_file):
+    conf = {}
+    with open(config_file) as fh:
+        for line in fh:
+            line = line.strip()
+            if len(line) != 0 and line[0] != "#":
+                parameter, value = line.strip().split('=', 1)
+                conf[parameter] = value.strip()
+    return conf
+
+
+#############################################
+def timedSender(jsondata, producer, pilotsNumbers, deltaTime):
+    if deltaTime > 10 or deltaTime <= 0:
+        deltaTime = 0.1
+    elif deltaTime > 0: 
+        time.sleep(float(deltaTime))
+        sendToKafka(jsondata, producer, pilotsNumbers)    
+
+
+def roundFloat(timeOfJson_str):
+    actualTime = ISOToFloat(timeOfJson_str)
+    decimal = round(actualTime, 3)
+    limitedDecimal = "{:.3f}".format(decimal)
+    actualTime = float(limitedDecimal)
+    return actualTime
+
+
+def readJsonFile():
+    filejson = open("rawMiami.json", "r")
+    jsondata = filejson.read()
+    filejson.close()  # funzione per preparare il file
+    jsondata = jsondata.replace("}{", "}£{")
+    jsondatalist = jsondata.split("£")
+    return jsondatalist
+
+
+def makePilotList(pilotsRaw):
+
+    pilots = []
+    for i in range(20):
+        if pilotsRaw[i]["permanentNumber"] == "33":
+            pilots.append(1)
+        else:
+            pilots.append(int(pilotsRaw[i]["permanentNumber"]))
+    return pilots
+
+
+def getPilotsData():
+    dbUrl = "http://ergast.com/api/f1/2023/drivers.json"
+    PilotData = req.get(dbUrl)
+    PilotJson = (json.loads(PilotData.text))["MRData"]["DriverTable"]["Drivers"]
+    pilotList = makePilotList(PilotJson)
+
+    return pilotList
+
+
+def sendToKafka(data, producer, pilotsNumbers):
+
+    try:
+        keys = dict(data["M"][0]["A"][1]["Lines"]).keys()
+        keys = str(keys)
+    except:
+        keys = ""
+    dataString = str(data)
+    for pilotNumber in pilotsNumbers:
+        if dataString.find("'R'") == -1 and keys.find("'"+str(pilotNumber)+"'") != -1:
+            #pilotinfo = data["M"][0]["A"][1]["Lines"][str(pilotNumber)]
+            pilotinfo=jsonModifier(data,pilotsNumber=pilotNumber,recapBool=False)
+            print("mando  " + str(pilotinfo))
+            producer.produce("LiveTimingData", key="info", value=str(pilotinfo).encode())
+            producer.flush()
+        elif dataString.find("'R'") != -1:
+            #pilotinfo = data["R"]["TimingData"]["Lines"][str(pilotNumber)]
+            pilotinfo=jsonModifier(data,pilotsNumber=pilotNumber,recapBool=True)
+            print("mando  " + str(pilotinfo))
+            producer.produce("LiveTimingData", key="info", value=str(pilotinfo).encode())
+            producer.flush()
+        
+
+
+
+def ISOToFloat(datestring):
+    yourdate = parser.parse(datestring)
+    splitDate = str(yourdate).split(' ')[1].split('+')[0].split(':')
+    year = splitDate[0]
+    month = splitDate[1]
+    day = splitDate[2]
+    floatDate = float(year+month+day)
+    return floatDate
+
+
+def Start():
+    print("Start")
+    jsondatalist = readJsonFile()
+    #pilotsNumbers = getPilotsData()
+    
+    pilotsNumbers = [1, 16, 55, 4, 10, 11, 14, 18, 20, 22, 23, 24, 27, 31, 44, 63, 77, 81, 2, 21]
+
+    producer = Producer(read_ccloud_config("client.properties"))       
+                  
+    previousTime = -40
+    deltaTime = 0
+
+    for jsondata in jsondatalist:
+        jsond = json.loads(jsondata)
+        boolean = (str(jsond)).find("'R'")
+
+        if boolean == -1:
+            actualTime = roundFloat(jsond["M"][0]["A"][2])
+            if previousTime == -40:
+                previousTime = actualTime
+            # delta tra precedente e attuale
+            deltaTime = actualTime-previousTime
+            previousTime = actualTime
+            timedSender(jsond, producer, pilotsNumbers, deltaTime)
+
+        else:
+            timedSender(jsond, producer, pilotsNumbers, 0.001)
+
+    print("End")
+
+
+def main():
+    try:
+        Start()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt has been caught.")
+        exit()    
+
+
+if __name__ == "__main__":
+    main()
---
+++ SPARK/python/testPySpark.py
@@ -0,0 +1,105 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+from pyspark.sql import SparkSession
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml import Pipeline
+
+# Definisci lo schema dei dati di input
+laptime_schema = StructType([
+    StructField("PilotNumber", IntegerType(), nullable=False),
+    StructField("LastLapTime", StringType(), nullable=True),
+    StructField("Lap", IntegerType(), nullable=True)
+])
+
+
+def linearRegression(pilotNumber):
+    global lapTimeTotal_df
+    df=lapTimeTotal_df.where("PilotNumber = " + pilotNumber).selectExpr("Lap as Lap", "LastLapTime as LapTime")
+
+    vectorAssembler = VectorAssembler(inputCols=["Lap"],outputCol="features")   
+    lr = LinearRegression(featuresCol="features",labelCol="LapTime",predictionCol="predictedLapTime")
+    pipeline = Pipeline(stages=[vectorAssembler, lr])
+
+    (trainingData, testData) = df.randomSplit([0.8, 0.2], seed=42)
+    model = pipeline.fit(trainingData)
+
+    predictions = model.transform(testData)
+    predictions.select("Lap","LapTime","predictedLapTime").show()
+ 
+
+
+
+
+
+def updateLapTimeTotal(df : DataFrame, epoch_id):
+  
+    global lapTimeTotal_df
+    global LastLapTime_df
+    if df.count() != 0:
+        lapTimeTotal_df = lapTimeTotal_df.union(df)
+        lapTimeTotal_df.show()
+        limited_df = lapTimeTotal_df.orderBy("Lap").limit(25)
+        LastLapTime_df2 = limited_df.groupBy("PilotNumber").agg(max("Lap").alias("Lap"))
+        LastLapTime_df2 = LastLapTime_df2.join(limited_df, ["PilotNumber", "Lap"], "inner")
+        print("LastLapTime_df2")
+        LastLapTime_df2.show()
+    #lapTimeTotal_df.groupBy("PilotNumber").agg(min("LastLapTime").alias("BestLapTime")).show()
+    
+    
+
+def main():
+    spark = SparkSession.builder \
+        .appName("SparkF1") \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+    
+    global lapTimeTotal_df
+    lapTimeTotal_df = spark.createDataFrame(spark.sparkContext.emptyRDD(), laptime_schema)
+    global LastLapTime_df
+    LastLapTime_df = spark.createDataFrame(spark.sparkContext.emptyRDD(), laptime_schema)
+
+    #df = spark \
+        #.readStream \
+        #.format("kafka") \
+        #.option("kafka.bootstrap.servers", "192.168.1.122:9093") \
+        #.option("subscribe", "LiveTimingData") \
+        #.load()
+
+
+    df = (spark.readStream
+                    .format("kafka")
+                    .option("kafka.bootstrap.servers", "pkc-4nmjv.francecentral.azure.confluent.cloud:9092")
+                    .option("kafka.ssl.endpoint.identification.algorithm", "https")
+                    .option("kafka.sasl.mechanism", "PLAIN")
+                    .option("kafka.security.protocol", "SASL_SSL")
+                    .option("kafka.sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username='{}' password='{}';".format("OZK7A2B5EBU2OMWI", "ODBBNwyLTXqOxfE77h+FNLLFa7KB/LakW7HuivBZoFP1fkXevp4tTvgqIuhxFLpr"))
+                    .option("subscribe", "LiveTimingData")
+                    .option("kafka.client.id", "Databricks")
+                    .option("kafka.group.id", "new_group2")
+                    .option("spark.streaming.kafka.maxRatePerPartition", "5")
+                    .option("startingOffsets", "earliest")
+                    .option("kafka.session.timeout.ms", "10000")
+                    .load() )    
+  
+
+    df2 = df.select(col("value").cast("string").alias("json"))
+
+    laptime_df = df2.select(
+        get_json_object("json", "$.PilotNumber").cast(IntegerType()).alias("PilotNumber"),
+        get_json_object("json", "$.LastLapTime.Value").alias("LastLapTime"),
+        get_json_object("json", "$.NumberOfLaps").cast(IntegerType()).alias("Lap")
+    ).where("LastLapTime is not null")
+    
+    laptime_query = laptime_df.writeStream\
+        .outputMode("append")\
+        .foreachBatch(updateLapTimeTotal)\
+        .start()
+
+    laptime_query.awaitTermination()
+
+    #query = df.writeStream.outputMode("append").format("console").start().awaitTermination()
+
+if __name__ == "__main__":
+    main()
