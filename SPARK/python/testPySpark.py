e40396dda138a2fb59a57eb3ffd27282cd973a97
GiuseppeBnn <giusbon2@gmail.com>
2023-06-02 15:40:28 +0200

    testing new index

--- SPARK/python/testPySpark.py
+++ SPARK/python/testPySpark.py
@@ -3,7 +3,7 @@ from pyspark.sql.types import StructType, StructField, IntegerType, StringType, 
 from pyspark.sql import SparkSession
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.feature import VectorAssembler
-#from pyspark.ml.evaluation import RegressionEvaluator
+# from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml import Pipeline
 
 import elasticsearch
@@ -27,118 +27,120 @@ prevision_schema = StructType([
 lapTimeTotal_df = None
 LastLapTime_df = None
 complete_df = None
-es=elasticsearch.Elasticsearch(hosts=["http://elasticsearch:9200"])
-
+es = elasticsearch.Elasticsearch(hosts=["http://elasticsearch:9200"])
 
 
 mapping = {
-  "properties": {
-    "PilotNumber": {
-      "type": "integer"
-    },
-    "Lap": {
-        "type": "integer"
-    },
-    "NextLapTimePrediction": {
-        "type": "float"
+    "mappings": {
+        "properties": {
+            "PilotNumber": {
+                "type": "integer"
+            },
+            "Lap": {
+                "type": "integer"
+            },
+            "NextLapTimePrediction": {
+                "type": "float"
+            }
+        }
     }
-  }
 }
 es.indices.create(index="predictions", body=mapping)
 
 
+def sendToES(data: DataFrame, choose: int):
 
-
-
-def sendToES(data:DataFrame, choose:int):
-    
     global es
-    if(choose==1):
-        data_json=data.toJSON().collect()
+    if (choose == 1):
+        data_json = data.toJSON().collect()
         for d in data_json:
-            #sendo to elasticsearch with d as float
-            es.index(index="predictions",body=d)
-            
-            print(d,type(d))
-    if(choose==2):
-        data_json=data.toJSON().collect()
+            # sendo to elasticsearch with d as float
+            es.index(index="predictions", body=d)
+
+            print(d, type(d))
+    if (choose == 2):
+        data_json = data.toJSON().collect()
         for d in data_json:
-            es.index(index="laptimes",body=d)       
-        
-    
+            es.index(index="laptimes", body=d)
+
+
 def linearRegression(pilotNumber):
     global lapTimeTotal_df
     global LastLapTime_df
     global complete_df
-    df = lapTimeTotal_df.where("PilotNumber = " + pilotNumber).selectExpr("PilotNumber as PilotNumber","Lap as Lap", "LastLapTime as LapTime")
+    df = lapTimeTotal_df.where("PilotNumber = " + pilotNumber).selectExpr(
+        "PilotNumber as PilotNumber", "Lap as Lap", "LastLapTime as LapTime")
     print("Dataframe del pilota " + pilotNumber)
-    df = df.withColumn("Seconds", (split(col("LapTime"), ":").getItem(0) * 60 + split(col("LapTime"), ":").getItem(1)))
+    df = df.withColumn("Seconds", (split(col("LapTime"), ":").getItem(
+        0) * 60 + split(col("LapTime"), ":").getItem(1)))
     df = df.withColumn("Seconds", df["Seconds"].cast(FloatType()))
-    df=df.select("Lap","Seconds")
+    df = df.select("Lap", "Seconds")
     vectorAssembler = VectorAssembler(inputCols=["Lap"], outputCol="features")
-    lr = LinearRegression(featuresCol="features", regParam=0.01,labelCol="Seconds")
+    lr = LinearRegression(featuresCol="features",
+                          regParam=0.01, labelCol="Seconds")
     pipeline = Pipeline(stages=[vectorAssembler, lr])
-    spark20=SparkSession.builder.appName("SparkF1").getOrCreate()
-    NextLap=df.agg(max("Lap").alias("Lap")).collect()
-    if(NextLap[0]["Lap"] is None):
-        NextLap=1
+    spark20 = SparkSession.builder.appName("SparkF1").getOrCreate()
+    NextLap = df.agg(max("Lap").alias("Lap")).collect()
+    if (NextLap[0]["Lap"] is None):
+        NextLap = 1
     else:
-        NextLap=NextLap[0]["Lap"]+1    
-    NextLap_df = spark20.createDataFrame([(pilotNumber,NextLap, 0)],["PilotNumber","Lap","Seconds"])
-    
+        NextLap = NextLap[0]["Lap"]+1
+    NextLap_df = spark20.createDataFrame([(pilotNumber, NextLap, 0)], [
+                                         "PilotNumber", "Lap", "Seconds"])
+
     model = pipeline.fit(df)
 
     predictions = model.transform(NextLap_df)
-   
-    #predictions = predictions.withColumn("prediction", concat( lit(floor(col("prediction")/60)), lit(":"), format_number((col("prediction")%60), 3)))
-    #predictions = predictions.withColumn("prediction", predictions["prediction"].cast(StringType()))
-    predictions= predictions.selectExpr("PilotNumber as PilotNumber","Lap as NextLap","prediction as NextLapTimePrediction")
-    predictions = predictions.withColumn("NextLapTimePrediction", predictions["NextLapTimePrediction"].cast(FloatType()))
-    #predictions.show()
-    sendToES(predictions,1)
-    
+
+    # predictions = predictions.withColumn("prediction", concat( lit(floor(col("prediction")/60)), lit(":"), format_number((col("prediction")%60), 3)))
+    # predictions = predictions.withColumn("prediction", predictions["prediction"].cast(StringType()))
+    predictions = predictions.selectExpr(
+        "PilotNumber as PilotNumber", "Lap as NextLap", "prediction as NextLapTimePrediction")
+    predictions = predictions.withColumn(
+        "NextLapTimePrediction", predictions["NextLapTimePrediction"].cast(FloatType()))
+    # predictions.show()
+    sendToES(predictions, 1)
+
     print("mando a ES")
-    
-
- 
 
 
+def updateLapTimeTotal(df: DataFrame, epoch_id):
 
-
-
-def updateLapTimeTotal(df : DataFrame, epoch_id):
-  
     global lapTimeTotal_df
     global LastLapTime_df
     if df.count() != 0:
         lapTimeTotal_df = lapTimeTotal_df.union(df)
-        #lapTimeTotal_df.show()
+        # lapTimeTotal_df.show()
         limited_df = lapTimeTotal_df.limit(30)
-        LastLapTime_df2 = limited_df.groupBy("PilotNumber").agg(max("Lap").alias("Lap"))
-        LastLapTime_df2 = LastLapTime_df2.join(limited_df, ["PilotNumber", "Lap"], "inner")
-        sendToES(LastLapTime_df2,2)
-        #print("Latest Lap Time per Pilot")
-        #LastLapTime_df2.show()
+        LastLapTime_df2 = limited_df.groupBy(
+            "PilotNumber").agg(max("Lap").alias("Lap"))
+        LastLapTime_df2 = LastLapTime_df2.join(
+            limited_df, ["PilotNumber", "Lap"], "inner")
+        sendToES(LastLapTime_df2, 2)
+        # print("Latest Lap Time per Pilot")
+        # LastLapTime_df2.show()
 
         for row in df.collect():
             linearRegression(str(row.PilotNumber))
-    
-    
+
 
 def main():
     spark = SparkSession.builder \
         .appName("SparkF1") \
         .getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
-    
-    global lapTimeTotal_df    
-    lapTimeTotal_df = spark.createDataFrame(spark.sparkContext.emptyRDD(), laptime_schema)
 
-    global LastLapTime_df  
-    LastLapTime_df = spark.createDataFrame(spark.sparkContext.emptyRDD(), laptime_schema)
+    global lapTimeTotal_df
+    lapTimeTotal_df = spark.createDataFrame(
+        spark.sparkContext.emptyRDD(), laptime_schema)
+
+    global LastLapTime_df
+    LastLapTime_df = spark.createDataFrame(
+        spark.sparkContext.emptyRDD(), laptime_schema)
 
     global complete_df
-    complete_df = spark.createDataFrame(spark.sparkContext.emptyRDD(), complete_schema)
+    complete_df = spark.createDataFrame(
+        spark.sparkContext.emptyRDD(), complete_schema)
     df = spark \
         .readStream \
         .format("kafka") \
@@ -146,8 +148,7 @@ def main():
         .option("subscribe", "LiveTimingData") \
         .load()
 
-
-    #df = (spark.readStream
+    # df = (spark.readStream
     #                .format("kafka")
     #                .option("kafka.bootstrap.servers", "pkc-4nmjv.francecentral.azure.confluent.cloud:9092")
     #                .option("kafka.ssl.endpoint.identification.algorithm", "https")
@@ -159,17 +160,18 @@ def main():
     #                .option("spark.streaming.kafka.maxRatePerPartition", "5")
     #                .option("startingOffsets", "latest")
     #                .option("kafka.session.timeout.ms", "10000")
-    #                .load() )    
-  
+    #                .load() )
 
     df2 = df.select(col("value").cast("string").alias("json"))
 
     laptime_df = df2.select(
-        get_json_object("json", "$.PilotNumber").cast(IntegerType()).alias("PilotNumber"),
+        get_json_object("json", "$.PilotNumber").cast(
+            IntegerType()).alias("PilotNumber"),
         get_json_object("json", "$.LastLapTime.Value").alias("LastLapTime"),
-        get_json_object("json", "$.NumberOfLaps").cast(IntegerType()).alias("Lap")
+        get_json_object("json", "$.NumberOfLaps").cast(
+            IntegerType()).alias("Lap")
     ).where("LastLapTime is not null")
-    
+
     laptime_query = laptime_df.writeStream\
         .outputMode("append")\
         .foreachBatch(updateLapTimeTotal)\
@@ -177,7 +179,8 @@ def main():
 
     laptime_query.awaitTermination()
 
-    #query = df.writeStream.outputMode("append").format("console").start().awaitTermination()
+    # query = df.writeStream.outputMode("append").format("console").start().awaitTermination()
+
 
 if __name__ == "__main__":
     main()
