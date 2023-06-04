a3cff7c1e0162ac536bc265c66dc00d70992ea90
GiuseppeBnn <giusbon2@gmail.com>
2023-06-04 12:24:00 +0200

    testing fix

--- final-compose/python/testPySpark.py
+++ final-compose/python/testPySpark.py
@@ -162,7 +162,7 @@ def main():
         get_json_object("json", "$.LastLapTime.Value").alias("LastLapTime"),
         get_json_object("json", "$.NumberOfLaps").cast(
             IntegerType()).alias("Lap")
-    ).where("Lap is not null")
+    ).where("Lap is not null and LastLapTime is not null")
 
     laptime_query = laptime_df.writeStream\
         .outputMode("append")\
