9381737cbfc14f23846ff871f4126469daf7e8fd
GiuseppeBnn <giusbon2@gmail.com>
2023-06-10 18:14:09 +0200

    optimized performance

--- f1-live/pySpark/SparkRegression.py
+++ f1-live/pySpark/SparkRegression.py
@@ -1,5 +1,5 @@
 from pyspark.sql.functions import *
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, FloatType, TimestampType
+from pyspark.sql.types import StructType, StructField, IntegerType, FloatType, StringType
 from pyspark.sql import SparkSession
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.feature import VectorAssembler
@@ -7,6 +7,7 @@ from pyspark.ml import Pipeline
 import requests as req
 import json
 import elasticsearch
+import pandas as pd
 
 
 es = elasticsearch.Elasticsearch(hosts=["http://elasticsearch:9200"])
@@ -18,7 +19,7 @@ laptime_schema = StructType([
     StructField("PilotNumber", IntegerType(), True),
     StructField("Lap", IntegerType(), True),
     StructField("Seconds", FloatType(), True),
-    StructField("@timestamp", TimestampType(), True)
+    StructField("@timestamp", StringType(), True)
 ])
 prediction_schema = StructType([
     StructField("PilotN", IntegerType(), True),
@@ -38,22 +39,23 @@ def linearRegression(pilotNumber):
     global pilotDataframes
     global pipeline
     global pilotModels
+    
     df = pilotDataframes[pilotNumber]
-    if df.count() > 0:
-        NextLap = df.agg(max("Lap")).collect()[0][0] + 1
-        #df.show()
-        if(int(NextLap)%5==0 or pilotModels[pilotNumber]==0 or int(NextLap)<6):
-            pilotModels[pilotNumber] = pipeline.fit(df)        
+    next_lap = df["Lap"].max() + 1
+    spark_session = SparkSession.builder.appName("SparkF1").getOrCreate()
+    print(df)
+    if(int(next_lap)%2==0 or pilotModels[pilotNumber]==0 or int(next_lap)<6):
+        df= spark_session.createDataFrame(df, laptime_schema)
+        pilotModels[pilotNumber] = pipeline.fit(df)   
+        df.unpersist()
 
-        model = pilotModels[pilotNumber]
-        spark_session = SparkSession.builder.appName("SparkF1").getOrCreate()
-        NextLap_df = spark_session.createDataFrame([(pilotNumber, NextLap)], prediction_schema).cache()
-        predictions = model.transform(NextLap_df).withColumn("prediction", col("prediction").cast(FloatType())).cache()
-        NextLap_df.unpersist()
-
-        predictions = predictions.withColumnRenamed("Lap", "NextLap").withColumn("@timestamp", current_timestamp())
-        #predictions.show()
-        sendToES(predictions, 1)
+    model = pilotModels[pilotNumber]
+    NextLap_df = spark_session.createDataFrame([(pilotNumber, next_lap)], prediction_schema).cache()
+    predictions = model.transform(NextLap_df).withColumn("prediction", col("prediction").cast(FloatType())).cache()
+    NextLap_df.unpersist()
+    predictions = predictions.withColumnRenamed("Lap", "NextLap").withColumn("@timestamp", current_timestamp())
+    predictions.show()
+    sendToES(predictions, 1)
         
 #crea una lista di piloti
 #viene sostituito il numero del 33 (Verstappen) con 1 (perchè db non aggiornato bene)
@@ -81,31 +83,28 @@ def getPilotsData():
 def preparePilotsDataframes():
     pilotList = getPilotsData()
     global pilotDataframes
-    session=SparkSession.builder.appName("SparkF1").getOrCreate()
     for i in range(20):
-        pilotDataframes[pilotList[i]] = session.createDataFrame(
-            session.sparkContext.emptyRDD(), laptime_schema)
-        
+        pilotDataframes[pilotList[i]] = pd.DataFrame(
+            columns=["PilotNumber", "Lap", "Seconds"])
 
  #invia i dati a elasticsearch in formato json in base al parametro choose
  #choose=1 invia i dati di predizione
  #choose=2 invia i dati di lastlaptime
-def sendToES(data : DataFrame, choose: int):
+def sendToES(data, choose: int):
     global es
     if (choose == 1):
         data_json = data.toJSON().collect()
         data.unpersist()
-        print(data_json)
+        #print(data_json)
         for d in data_json:
             # sendo to elasticsearch with d as float
-            es.index(index="predictions", body=d)
-
-            #print(d, type(d))
+            es.index(index="predictions", document=d)
     if (choose == 2):
-        data_json = data.toJSON().collect()
-        print(data_json)
-        for d in data_json:
-            es.index(index="lastlaptimes", body=d)
+        #in questo caso data è un dataframe pandas
+        data_json = data.to_json(orient="records")
+        #rimuove dal json il carattere iniziale e finale messi dalla funzione to_json (slice)
+        data_json = data_json[1:-1]
+        es.index(index="lastlaptimes", document=data_json)
     
 
 #aggiorna l'ultimo giro dei piloti man mano che questi completano un giro
@@ -113,25 +112,26 @@ def sendToES(data : DataFrame, choose: int):
 #la cache permette di risolvere i problemi di performance dovuti al fatto che i dataframe vengono
 #ricreati ad ogni batch
 
-def updateLapTimeTotal_df(df : DataFrame, epoch_id):
+def updateLapTimeTotal_df(df: DataFrame, epoch_id):
     global pilotDataframes
     if df.count() > 0:
-        df=df.cache()
+        pandas_df=df.toPandas()    
         print("New batch arrived")
-        rows=df.rdd.collect()
-        for row in rows:
-            pn=row.PilotNumber
-            old_df=pilotDataframes[pn]
-            df2=df.filter(df.PilotNumber==pn)
-            pilotDataframes[pn] = old_df.union(df2).orderBy("Lap", ascending=False).limit(5).cache()
-            old_df.unpersist()
-            #pilotDataframes[pn]=(pilotDataframes[pn].orderBy("Lap", ascending=False).limit(5))
-            linearRegression(pn)
+        
+        for _, row in pandas_df.iterrows():
+            pn = row['PilotNumber']
+            old_df = pilotDataframes[pn]
+            df2 = pandas_df[pandas_df['PilotNumber'] == pn]
+            pilotDataframes[pn] = pd.concat([old_df, df2]).sort_values('Lap', ascending=False).head(10).copy()
             sendToES(df2, 2)
-    df.unpersist()  
+            linearRegression(pn)
+        
 
 def main():
     global pipeline
+
+
+    
 
     spark = SparkSession.builder \
         .appName("SparkF1") \
@@ -143,6 +143,12 @@ def main():
     preparePilotsDataframes()
     preparePilotModels()
 
+    vectorAssembler = VectorAssembler(inputCols=["Lap"], outputCol="features", handleInvalid="skip")
+    lr = LinearRegression(featuresCol="features",
+                          regParam=0.01, labelCol="Seconds", maxIter=20)
+    pipeline = Pipeline(stages=[vectorAssembler, lr])
+    print("Pipeline creata"+str(type(pipeline)))
+
     df = spark \
         .readStream \
         .format("kafka") \
@@ -151,13 +157,7 @@ def main():
         .option("subscribe", "LiveTimingData") \
         .option("startingOffsets", "latest") \
         .load()
-        
-    
-    vectorAssembler = VectorAssembler(inputCols=["Lap"], outputCol="features", handleInvalid="skip")
-    lr = LinearRegression(featuresCol="features",
-                          regParam=0.01, labelCol="Seconds", maxIter=6)
-    pipeline = Pipeline(stages=[vectorAssembler, lr])
-    print("Pipeline creata"+str(type(pipeline)))
+
 
     #opzione per leggere da kafka da confluent cloud
     # df = (spark.readStream
@@ -174,9 +174,7 @@ def main():
     #                .option("kafka.session.timeout.ms", "10000")
     #                .load() )
 
-    df2 = df.select(col("value").cast("string").alias("json"))
-
-    laptime_df = df2.select(
+    df2 = df.select(col("value").cast("string").alias("json")).select(
         get_json_object("json", "$.TimingData.PilotNumber").cast(
             IntegerType()).alias("PilotNumber"),
         get_json_object("json", "$.TimingData.NumberOfLaps").cast(
@@ -186,9 +184,7 @@ def main():
         get_json_object("json", "$.@timestamp").alias("@timestamp")   
 
     ).where("Lap is not null and Seconds is not null")
-
-    laptime_df = laptime_df.repartition("PilotNumber")
-    laptime_query = laptime_df.writeStream\
+    laptime_query = df2.writeStream\
         .outputMode("append").foreachBatch(updateLapTimeTotal_df).start()
     laptime_query.awaitTermination()
 
